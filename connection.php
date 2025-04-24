1bce761ea00c46a9ee2c16ae1756741f284177f2
Karen Chahrour <karenchahrour123@gmail.com>
2025-04-24 21:03:33 +0300

    Assigning the username to root and password to empty string

--- connection.php
+++ connection.php
@@ -2,9 +2,10 @@
 // use try and catch error block to prevent crashing and stopping the execution
 try{
     $host = "localhost";   // type of the host
-    $port = 3306;
-    $dbname = "quizdb";
-    
+    $port = 3306; // port number of mysql
+    $dbname = "quizdb"; //db name
+    $user = "root";
+    $pass = "";
 
 }catch(){
 
