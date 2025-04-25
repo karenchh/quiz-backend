0534299500ef13f03f32ba5cd0d06bc4adbe17fc
Karen Chahrour <karenchahrour123@gmail.com>
2025-04-25 18:39:31 +0300

    Inserting the user to users table in case of new user

--- register.php
+++ register.php
@@ -3,15 +3,14 @@ include 'connection.php';
 //Check if they are valid 
 if (isset($_POST["name"],$_POST["lastname"],$_POST["email"],$_POST["password"])){
     //Retriving the name , lastname, email and password from url by get statment
-    $username = $_POST["name"];
+    $name = $_POST["name"];
     $lastname = $_POST["lastname"];
     $email = $_POST["email"];
     $password = $_POST["password"];
 
-    $query = $connection->prepare("SELECT * FROM users WHERE email = :email AND password = :password");
+    $query = $connection->prepare("SELECT * FROM users WHERE email = :email");
 
     $query->bindValue(":email", $email, PDO::PARAM_STR);  // Binding the email to the email to prevent sql injection
-    $query->bindValue(":password", $password, PDO::PARAM_STR); 
         
     $query->execute();
 
@@ -24,7 +23,17 @@ if (isset($_POST["name"],$_POST["lastname"],$_POST["email"],$_POST["password"]))
     if($user == true){   // if the user exists
         echo json_encode(["message" => "Email already exists"]);
     }
-    
+    else{
+        $insertuserquery = $connection->prepare("INSERT INTO users (name,last_name,email,password) VALUES (:name,:lastname,:email,:password)");
+        $insertuserquery->bindValue(":name", $name, PDO::PARAM_STR);
+        $insertuserquery->bindValue(":lastname", $lastname, PDO::PARAM_STR);
+        $insertuserquery->bindValue(":email", $email, PDO::PARAM_STR);
+        $insertuserquery->bindValue(":password", $password, PDO::PARAM_STR);
+        $insertuserquery->execute();
+    }
+}
+else {
+    echo json_encode(["message" => "Registration failed invalid credintials."]);
 }
 
 
