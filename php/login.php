f291a9f4913bbbbba8b3e3f6f2a5d0574841fe0e
Karen Chahrour <karenchahrour123@gmail.com>
2025-04-25 19:23:47 +0300

    Fetching if the email exists and if the email and password matches to login

--- php/login.php
+++ php/login.php
@@ -9,12 +9,20 @@ if (isset($_POST["email"],$_POST["password"])){
     $query = $connection->prepare("SELECT * FROM users WHERE email = :email");
 
     $query->bindValue(":email", $email, PDO::PARAM_STR);  // Binding the email to the email to prevent sql injection
-    // Creating empty array
-    $result =[];
-
     $query->execute();
 
-
+    $user = $query->fetch(PDO::FETCH_ASSOC);
+    if($user == true && $user["password"] === $password){   // if the user exists check the password
+        if($user["email"] === "admin@quiz.com" && $user["password"] === 'admin123'){
+            echo json_encode(["message" => "You are an admin"]); // must be directed to the dashboard
+        }
+        else{
+            echo json_encode(["message" => "Logged in successfully"]); // must be redirected to the index.html home page
+        }
+    }
+    else{
+        echo json_encode(["message" => "User doesn't exist please sign up and try to login again"]); // or invalid email and password
+    }
 }
 else{
     echo json_encode(["message" => "login failed missing credintials."]);
