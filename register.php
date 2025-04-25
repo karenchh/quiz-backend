3bb12a14300fca41b7cf0c4947bada06870a68b2
Karen Chahrour <karenchahrour123@gmail.com>
2025-04-25 16:01:04 +0300

    Changing the method to post for more security and checking if values are set

--- register.php
+++ register.php
@@ -1,6 +1,13 @@
 <?php // declare it as a php file
 include 'connection.php';
-$username = $_GET["name"];
-$lastname = $_GET["lastname"];
-$email = $_GET["email"];
-$password = $_GET["password"];
+//Check if they are valid 
+if (isset($_POST["name"],$_POST["lastname"],$_POST["email"],$_POST["password"])){
+    //Retriving the name , lastname, email and password from url by get statment
+    $username = $_POST["name"];
+    $lastname = $_POST["lastname"];
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+}
+
+
+
