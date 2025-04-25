4107766ee762ccffe2cf1dc11dc76d40ded429fd
Karen Chahrour <karenchahrour123@gmail.com>
2025-04-25 19:28:33 +0300

    Retrieving the title and the description of the quiz from the input

--- php/createquiz.php
+++ php/createquiz.php
@@ -1,2 +1,8 @@
 <?php
 include 'connection.php';
+
+if (isset($_GET["title"],$_GET["description"])){
+    //Retriving title and description from url by get statment
+    $title = $_GET["title"];
+    $description = $_GET["description"];
+}
