5f7aae35e8f0c33ced37a132afbe0feb7d1d987f
Karen Chahrour <karenchahrour123@gmail.com>
2025-04-25 21:30:07 +0300

    Binding the quiz id with its value'

--- php/deletequiz.php
+++ php/deletequiz.php
@@ -7,6 +7,8 @@ if (isset($_POST["title"],$_POST["description"])){
     $title = $_POST["title"];
     $description = $_POST["description"];
     $query = $connection->prepare("DELETE FROM quizzes WHERE id = :id");
+
+    $query->bindValue(":id", $quiz_id ,PDO::PARAM_INT);
 }
 else{
     echo json_encode(["message" => "Quiz update failed"]);
