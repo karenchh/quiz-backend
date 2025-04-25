3817f8f52dc88b543b5e47622e3acd971c678555
Karen Chahrour <karenchahrour123@gmail.com>
2025-04-25 19:31:02 +0300

    Preparing the insert quiz query

--- php/createquiz.php
+++ php/createquiz.php
@@ -5,4 +5,6 @@ if (isset($_GET["title"],$_GET["description"])){
     //Retriving title and description from url by get statment
     $title = $_GET["title"];
     $description = $_GET["description"];
+
+    $query = $connection->prepare("INSERT INTO quizzes (title,description) VALUES (:title,:description)");
 }
