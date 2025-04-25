67b28808960e72dd397287e3cb08351387e8db9c
Karen Chahrour <karenchahrour123@gmail.com>
2025-04-25 21:27:34 +0300

    Checking if inputs are set and retrieving them by post

--- php/deletequiz.php
+++ php/deletequiz.php
@@ -1,3 +1,12 @@
 <?php
 include 'connection.php';
 $quiz_id = $_GET["id"];
+
+if (isset($_POST["title"],$_POST["description"])){
+    //Retriving title and description from url by get statment
+    $title = $_POST["title"];
+    $description = $_POST["description"];
+}
+else{
+    
+}
