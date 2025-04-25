c8f619ecd21f983032c29be689c17a0beba2734e
Karen Chahrour <karenchahrour123@gmail.com>
2025-04-25 18:52:47 +0300

    Retrieving the email and password from the url coming from the register form by post method

--- php/login.php
+++ php/login.php
@@ -1,2 +1,8 @@
 <?php
 include 'connection.php';
+
+if (isset($_POST["email"],$_POST["password"])){
+    //Retriving email and password from url by post statment
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+}
--- php/register.php
+++ php/register.php
@@ -2,7 +2,7 @@
 include 'connection.php';
 //Check if they are valid 
 if (isset($_POST["name"],$_POST["lastname"],$_POST["email"],$_POST["password"])){
-    //Retriving the name , lastname, email and password from url by get statment
+    //Retriving the name , lastname, email and password from url by post statment
     $name = $_POST["name"];
     $lastname = $_POST["lastname"];
     $email = $_POST["email"];
