099ff0aed6c7a6de3c9d06e063499de60fdd8575
Karen Chahrour <karenchahrour123@gmail.com>
2025-04-25 19:54:17 +0300

    Fetching the result of the query and appending each row to the results array

--- php/getallquizzes.php
+++ php/getallquizzes.php
@@ -5,3 +5,7 @@ $getquery = $connection->prepare("SELECT * FROM quizzes");
 $getquery->execute();
 
 $result = [];
+//Creating empty array since fetch points only to one row array and this query will retrieve more than one row/record
+while($user = $getquery->fetch(PDO::FETCH_ASSOC)){
+    $result [] = $user; // pushing to the array results
+}
