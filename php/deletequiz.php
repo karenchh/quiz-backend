0582b685f064f203c416f522ab073e4c75bd015d
Karen Chahrour <karenchahrour123@gmail.com>
2025-04-25 21:34:18 +0300

    Deleting the input since we are not in need for them we are just deleting a quiz

--- php/deletequiz.php
+++ php/deletequiz.php
@@ -1,17 +1,8 @@
 <?php
 include 'connection.php';
 $quiz_id = $_GET["id"];
+$query = $connection->prepare("DELETE FROM quizzes WHERE id = :id");
+$query->bindValue(":id", $quiz_id ,PDO::PARAM_INT);
+$query->execute();
 
-if (isset($_POST["title"],$_POST["description"])){
-    //Retriving title and description from url by get statment
-    $title = $_POST["title"];
-    $description = $_POST["description"];
-    $query = $connection->prepare("DELETE FROM quizzes WHERE id = :id");
-
-    $query->bindValue(":id", $quiz_id ,PDO::PARAM_INT);
-
-    $query->execute();
-}
-else{
-    echo json_encode(["message" => "Quiz update failed"]);
-}
+echo json_encode(["message" => "Quiz is deleted successfully"]);
