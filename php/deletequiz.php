5be6005ff8a6cd59dbe9bb480c38a369d0032d5a
Karen Chahrour <karenchahrour123@gmail.com>
2025-04-25 21:30:53 +0300

    Executing the delete query

--- php/deletequiz.php
+++ php/deletequiz.php
@@ -9,6 +9,8 @@ if (isset($_POST["title"],$_POST["description"])){
     $query = $connection->prepare("DELETE FROM quizzes WHERE id = :id");
 
     $query->bindValue(":id", $quiz_id ,PDO::PARAM_INT);
+
+    $query->execute();
 }
 else{
     echo json_encode(["message" => "Quiz update failed"]);
