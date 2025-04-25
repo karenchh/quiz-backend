660a9a7327b45a22bf65059cddb1063ee63d6387
Karen Chahrour <karenchahrour123@gmail.com>
2025-04-25 21:06:01 +0300

    Fetching the quiz edit query

--- php/editquiz.php
+++ php/editquiz.php
@@ -1,6 +1,11 @@
 <?php
 include 'connection.php';
 $quiz_id = $_GET["id"];
-$query = $connection->prepare("SELECT * FROM quizzes WHERE id = :id");
+$query = $connection->prepare("SELECT title, description FROM quizzes WHERE id = :id");
 
 $query->bindValue(":id", $id ,PDO::PARAM_INT);
+
+$query->execute();
+
+$quiz = $query->fetch(PDO::FETCH_ASSOC);
+// since quiz id is unique
--- php/getallquizzes.php
+++ php/getallquizzes.php
@@ -6,7 +6,7 @@ $getquery->execute();
 
 $result = [];
 //Creating empty array since fetch points only to one row array and this query will retrieve more than one row/record
-while($user = $getquery->fetch(PDO::FETCH_ASSOC)){
-    $result [] = $user; // pushing to the array results
+while($quiz = $getquery->fetch(PDO::FETCH_ASSOC)){
+    $result [] = $quiz; // pushing to the array results
 }
 echo json_encode($result);
