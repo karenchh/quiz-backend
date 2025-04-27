c792a0d37372f3d681ff016e06c9390dfd10d547
Karen Chahrour <karenchahrour123@gmail.com>
2025-04-27 17:01:54 +0300

    Preparing the questions query

--- php/getquizquestions.php
+++ php/getquizquestions.php
@@ -1,3 +1,11 @@
 <?php
     include 'connection.php';
-    
+    $getquery = $connection->prepare("SELECT * FROM questions WHERE quiz_id = :quiz_id");
+    $getquery->execute();
+
+    $result = [];
+    //Creating empty array since fetch points only to one row array and this query will retrieve more than one row/record
+    while($quiz = $getquery->fetch(PDO::FETCH_ASSOC)){
+        $result [] = $quiz; // pushing to the array results
+    }
+    echo json_encode($result);
