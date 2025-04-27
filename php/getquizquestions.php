1a9dc3dbf659350e757c848954ac3a3b1001ee65
Karen Chahrour <karenchahrour123@gmail.com>
2025-04-27 17:03:33 +0300

    Retrieving the quiz_id

--- php/getquizquestions.php
+++ php/getquizquestions.php
@@ -1,11 +1,7 @@
 <?php
-    include 'connection.php';
+include 'connection.php';
+if (isset($_GET["quiz_id"])) {
+    // Retrieving quiz_id from GET 
+    $quiz_id = $_GET["quiz_id"];
     $getquery = $connection->prepare("SELECT * FROM questions WHERE quiz_id = :quiz_id");
     $getquery->execute();
-
-    $result = [];
-    //Creating empty array since fetch points only to one row array and this query will retrieve more than one row/record
-    while($quiz = $getquery->fetch(PDO::FETCH_ASSOC)){
-        $result [] = $quiz; // pushing to the array results
-    }
-    echo json_encode($result);
