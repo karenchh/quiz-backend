ca8c99125889cb61560747b0e3ca218d853354e2
Karen Chahrour <karenchahrour123@gmail.com>
2025-04-27 16:57:38 +0300

    Executing create quiz.php

--- php/createquiz.php
+++ php/createquiz.php
@@ -1,20 +1,21 @@
-<?php
-include 'connection.php';
-
-if (isset($_GET["title"],$_GET["description"])){
-    //Retriving title and description from url by get statment
-    $title = $_GET["title"];
-    $description = $_GET["description"];
-
-    $query = $connection->prepare("INSERT INTO quizzes (title,description) VALUES (:title,:description)");
-
-    $query->bindValue(":title", $title ,PDO::PARAM_STR);
-    $query->bindValue(":description", $description ,PDO::PARAM_STR);
-
-    $query->execute();
-
-    echo json_encode(["message" => "Quiz created successfully"]);
-}
-else{
-    echo json_encode(["message" => "Quiz not created ,please make sure you didn't miss any input"]);
-}
+    <?php
+    include 'connection.php';
+    
+    if (isset($_GET["quiz_id"], $_POST["question_text"])) {
+        // Retrieving quiz_id and question_text from GET and POST
+        $quiz_id = $_GET["quiz_id"];
+        $question_text = $_POST["question_text"];
+    
+        $query = $connection->prepare("INSERT INTO questions (quiz_id, question_text) VALUES (:quiz_id, :question_text)");
+    
+        $query->bindValue(":quiz_id", $quiz_id ,PDO::PARAM_INT);
+        $query->bindValue(":question_text", $question_text ,PDO::PARAM_STR);
+    
+        $query->execute();
+    
+        echo json_encode(["message" => "Question created successfully"]);
+    } else {
+        echo json_encode(["message" => "Question not created, please make sure you didn't miss any input"]);
+    }
+    ?>
+    
